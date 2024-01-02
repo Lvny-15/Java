49a9bbc55fdce4288ae939290cecc5d997feb9c0
Lvny-15 <155420583+Lvny-15@users.noreply.github.com>
2024-01-01 23:42:00 -0800

    Add files via upload

---
+++ EqualifnotLargest.java
@@ -0,0 +1,19 @@
+package Looping;
+
+public class EqualifnotLargest 
+{
+	public int Number(int x,int y,int z)
+	{
+		if(x == y && y==z)
+			{
+				System.out.println("given three numbers are equal");
+			}
+			else
+			{
+				System.out.println("largest number");
+			}
+		
+		return x;
+	}
+
+}
